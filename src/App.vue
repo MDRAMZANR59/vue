fbe506d7e750270424e1e478caabbf76e859765a
Md Akash <you@example.com>
2024-11-23 23:21:02 +0600

    Edit for use in react

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div>
+      <header></header>
+
+        <router-view/>
+
+      <footer></footer>
+    </div>
+  </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Header from './components/Include/Header.vue'
+  import Footer from './components/Include/Footer.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default{
+    name: 'App',
+    component:{
+      Header,Footer
+    }
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/router.js
+++ src/router.js
@@ -1,1 +1,17 @@
 import { createWebHistory, createRouter } from "vue-router";
+
+const routes=[
+    {
+        path:'/home',
+        asias:'/home',
+        name:'home',
+        component:()=> import('./components/Home.vue')
+    }
+];
+
+const router=createRouter({
+    history:createWebHistory(),
+    routes,
+});
+
+export default router;
