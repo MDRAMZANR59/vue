cf399dd39fc6e68c7bd473b1847752e2876794a7
Md Akash <you@example.com>
2024-11-26 23:07:21 +0600

    Edit Sidebar to daynamic

--- src/components/Include/Sidebar.vue
+++ src/components/Include/Sidebar.vue
@@ -1,7 +1,7 @@
 <template>
     <aside class="main-sidebar sidebar-dark-primary elevation-4" style="min-height:100vh">
         <a href="index3.html" class="brand-link">
-            <img src="../../../public/assets/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+            <img src="../../../public/assets/dist/img/logo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
             <span class="brand-text font-weight-light">AdminLTE 3</span>
         </a>
         <!-- Sidebar -->
@@ -30,7 +30,7 @@
             <nav class="mt-2">
                 <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                     <li class="nav-item menu-open">
-                        <a href="#" class="nav-link active">
+                        <a href="http://127.0.0.1:3000/" class="nav-link active">
                             <i class="nav-icon fas fa-tachometer-alt"></i>
                             <p>
                                 Dashboard
@@ -41,7 +41,7 @@
 
                     <li :class="['nav-item', { 'menu-open': openMenu === 'user' }]">
                         <a href="javascript:void(0)" class="nav-link" @click="handleMenuClick('user')">
-                            <i class="nav-icon fas fa-sticky-note"></i>
+                            <i class="nav-icon fas fa-user"></i>
                             <p>
                                 User
                                 <i class="fas fa-angle-left right"></i>
@@ -49,13 +49,13 @@
                         </a>
                         <ul class="nav nav-treeview">
                             <li class="nav-item">
-                                <a href="/customerNote/addNote" class="nav-link">
+                                <a href="http://127.0.0.1:3000/user/addUser" class="nav-link">
                                     <i class="far fa-circle nav-icon"></i>
                                     <p>Add User</p>
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a href="/customerNote/noteList" class="nav-link">
+                                <a href="http://127.0.0.1:3000/user/userList" class="nav-link">
                                     <i class="far fa-circle nav-icon"></i>
                                     <p>User List</p>
                                 </a>
@@ -72,13 +72,13 @@
                         </a>
                         <ul class="nav nav-treeview">
                             <li class="nav-item">
-                                <a href="/customerNote/addNote" class="nav-link">
+                                <a href="http://127.0.0.1:3000/customerNote/addNote" class="nav-link">
                                     <i class="far fa-circle nav-icon"></i>
                                     <p>Customer Note Add</p>
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a href="/customerNote/noteList" class="nav-link">
+                                <a href="http://127.0.0.1:3000/customer/customerList" class="nav-link">
                                     <i class="far fa-circle nav-icon"></i>
                                     <p>Note List</p>
                                 </a>
@@ -87,7 +87,7 @@
                     </li>
                     <li :class="['nav-item', { 'menu-open': openMenu === 'customer' }]">
                         <a href="javascript:void(0)" class="nav-link" @click="handleMenuClick('customer')">
-                            <i class="nav-icon fas fa-sticky-note"></i>
+                            <i class="nav-icon fas fa-restroom"></i>
                             <p>
                                 Customer's
                                 <i class="fas fa-angle-left right"></i>
@@ -95,13 +95,13 @@
                         </a>
                         <ul class="nav nav-treeview">
                             <li class="nav-item">
-                                <a href="/customerNote/addNote" class="nav-link">
+                                <a href="http://127.0.0.1:3000/customer/customerAdd" class="nav-link">
                                     <i class="far fa-circle nav-icon"></i>
                                     <p>Customer Add</p>
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a href="/customerNote/noteList" class="nav-link">
+                                <a href="http://127.0.0.1:3000/customer/customerList" class="nav-link">
                                     <i class="far fa-circle nav-icon"></i>
                                     <p>Customer List</p>
                                 </a>
@@ -110,7 +110,7 @@
                     </li>
                     <li :class="['nav-item', { 'menu-open': openMenu === 'project' }]">
                         <a href="javascript:void(0)" class="nav-link" @click="handleMenuClick('project')">
-                            <i class="nav-icon fas fa-sticky-note"></i>
+                            <i class="nav-icon fas fa-folder"></i>
                             <p>
                                 Project
                                 <i class="fas fa-angle-left right"></i>
@@ -118,22 +118,34 @@
                         </a>
                         <ul class="nav nav-treeview">
                             <li class="nav-item">
-                                <a href="/customerNote/addNote" class="nav-link">
+                                <a href="http://127.0.0.1:3000/project/projectAdd" class="nav-link">
                                     <i class="far fa-circle nav-icon"></i>
-                                    <p>Project Add</p>
+                                    <p>Add Project</p>
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a href="/customerNote/noteList" class="nav-link">
+                                <a href="http://127.0.0.1:3000/project/projectList" class="nav-link">
                                     <i class="far fa-circle nav-icon"></i>
                                     <p>Project List</p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="http://127.0.0.1:3000/project/DeliveryProject" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Delivary Project</p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="http://127.0.0.1:3000/project/CancalingProject" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Cancaled Project</p>
                                 </a>
                             </li>
                         </ul>
                     </li>
                     <li :class="['nav-item', { 'menu-open': openMenu === 'mail' }]">
                         <a href="javascript:void(0)" class="nav-link" @click="handleMenuClick('mail')">
-                            <i class="nav-icon fas fa-sticky-note"></i>
+                            <i class="nav-icon fas fa-envelope"></i>
                             <p>
                                 Mail
                                 <i class="fas fa-angle-left right"></i>
@@ -141,15 +153,9 @@
                         </a>
                         <ul class="nav nav-treeview">
                             <li class="nav-item">
-                                <a href="/customerNote/addNote" class="nav-link">
+                                <a href="http://127.0.0.1:3000/mail/mailBox" class="nav-link">
                                     <i class="far fa-circle nav-icon"></i>
                                     <p>Mail Box</p>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="/customerNote/noteList" class="nav-link">
-                                    <i class="far fa-circle nav-icon"></i>
-                                    <p>Note List</p>
                                 </a>
                             </li>
                         </ul>
@@ -157,33 +163,38 @@
 
                     <li class="nav-header">EXAMPLES</li>
                     <li class="nav-item">
-                        <a href="#" class="nav-link">
-                            <i class="nav-icon far fa-envelope"></i>
+                        <a href="http://127.0.0.1:3000/service/serviceAdd" class="nav-link">
+                            <i class="fas fa-circle nav-icon"></i>
                             <p>
                                 Service Add
-                                <i class="fas fa-angle-left right"></i>
                             </p>
                         </a>
                     </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">
-                            <i class="nav-icon far fa-envelope"></i>
+                    <li :class="['nav-item', { 'menu-open': openMenu === 'serviceList' }]">
+                        <a href="javascript:void(0)" class="nav-link" @click="handleMenuClick('serviceList')">
+                            <i class="nav-icon fas fa-envelope"></i>
                             <p>
-                                Mailbox
+                                Service List
                                 <i class="fas fa-angle-left right"></i>
                             </p>
                         </a>
                         <ul class="nav nav-treeview">
                             <li class="nav-item">
-                                <a href="pages/mailbox/mailbox.html" class="nav-link">
+                                <a href="http://127.0.0.1:3000/service/serviceList/AndroidAppDevolopment" class="nav-link">
                                     <i class="far fa-circle nav-icon"></i>
-                                    <p>Inbox</p>
+                                    <p>Android App Development</p>
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a href="pages/mailbox/compose.html" class="nav-link">
+                                <a href="http://127.0.0.1:3000/service/serviceList/WebAppDevolopment" class="nav-link">
                                     <i class="far fa-circle nav-icon"></i>
-                                    <p>Compose</p>
+                                    <p>Web Application Development</p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="http://127.0.0.1:3000/service/serviceList/PcAppDevolopment" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>PC App Development</p>
                                 </a>
                             </li>
                         </ul>
--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,71 +1,101 @@
 <template>
-    <div class="container-xxl py-5">
-      <div class="row justify-content-center">
-        <div class="col-6">
-          <h2 class="text-center text-primary mb-4">Login Form</h2>
-          <form method="post" class="border p-4 bg-light rounded">
-            <div class="mb-3">
-              <label htmlFor="email" class="form-label text-primary">Email</label>
-              <input type="email" class="form-control" id="email" name="email"
-                v-model="formData.email" placeholder="Enter Email Id"/>
-            </div>
-            
-            <div class="mb-3">
-              <label htmlFor="password" class="form-label text-primary">Password </label>
-              <input type="password" class="form-control" id="password" name="password"
-               v-model="formData.password" placeholder="Enter a password"/>
-            </div>
-            <button type="button" @click="login" class="btn btn-primary w-100"> Submit</button>
-          </form>
+  <div class="container-xxl py-5">
+    <div class="row justify-content-center">
+      <div class="col-6">
+        <div class="text-center mb-5">
+            <img src="../../public/assets/dist/img/logo.png" height="50" class='mb-4 rounded-circle'/>
+            <h3>Sign In</h3>
         </div>
+        <p class="login-box-msg">Sign in to start your session</p>
+        <form method="post">
+            <div class="input-group mb-3">
+                <input type="email" class="form-control" placeholder="Email" name="email" id="email" v-model="formData.email"/>
+                <div class="input-group-append">
+                    <div class="input-group-text">
+                        <span class="fas fa-envelope"></span>
+                    </div>
+                </div>
+            </div>
+            <div class="input-group mb-3">
+                <input v-model="formData.password" type="password" class="form-control" placeholder="Password" name="password" />
+                <div class="input-group-append">
+                    <div class="input-group-text">
+                        <span class="fas fa-lock"></span>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-8">
+                    <div class="icheck-primary">
+                    <input type="checkbox" id="remember"/>
+                    <label htmlFor="remember">
+                        Remember Me
+                    </label>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <button @click="login" type="button" class="btn btn-primary btn-block">Sign In</button>
+                </div>
+            </div>
+        </form>
+        <div class="social-auth-links text-center mb-3">
+            <p>- OR -</p>
+            <a href="#" class="btn btn-block btn-danger">
+                <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
+            </a>
+        </div>
+        <p class="mb-1">
+            <Link to="#">I forgot my password</Link>
+        </p>
       </div>
     </div>
-  </template>
-  
-  <script>
-    import DataService from "../services/DataService";
-    import router from '@/router';
-  export default {
-    name: "LoginForm",
-    data() {
-      return {
-        formData: {
-            email: "",
-          password: "",
-        },
+  </div>
+</template>
+
+<script>
+  import DataService from "../services/DataService";
+  import router from '@/router';
+export default {
+  name: "LoginForm",
+  data() {
+    return {
+      formData: {
+          email: "",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    login() {
+      var data = {
+          email: this.formData.email,
+        password: this.formData.password
       };
-    },
-    methods: {
-      login() {
-        var data = {
-            email: this.formData.email,
-          password: this.formData.password
-        };
-        DataService.login(data)
-        .then(response => {
-          console.log(response.data.data.token)
-          if(response.data.data.token)
-            sessionStorage.setItem('uid', response.data.data.token);
-          else
-            alert(response.data.error)
-            
-            router.push({ name: 'home' });
-            window.location.href='/home';
-        })
-        .catch(e => {
-          console.log(e);
-        });
-      }
-    },
-  };
+      DataService.login(data)
+      .then(response => {
+        console.log(response.data.data.token)
+        if(response.data.data.token)
+          sessionStorage.setItem('uid', response.data.data.token);
+        else
+          alert(response.data.error)
+          
+          router.push({ name: 'home' });
+          window.location.href='/home';
+      })
+      .catch(e => {
+        console.log(e);
+      });
+    }
+  },
+};
 </script>
-  
- <style scoped>
-  .container {
-    max-width: 400px;
-    margin: 0 auto;
-  }
-  .text-center {
-    text-align: center;
-  }
-  </style>
+
+<style scoped>
+.container {
+  max-width: 400px;
+  margin: 0 auto;
+}
+.text-center {
+  text-align: center;
+}
+</style>
