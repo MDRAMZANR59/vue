aa2d60d8f4192a482fb462c8bd57488c60a056a6
Md Akash <you@example.com>
2024-11-30 01:01:16 +0600

    Edit Dashboard Chart

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,130 +1,142 @@
-<!-- <template>
-     <div class="content">
-      <div class="content-header">
-        <div class="container-fluid">
-          <div class="row mb-2">
-            <div class="col-sm-6">
-              <h1 class="m-0">Dashboard</h1>
-            </div>
-            <div class="col-sm-6">
-              <ol class="breadcrumb float-sm-right">
-                <li class="breadcrumb-item"><a href="#">Home</a></li>
-                <li class="breadcrumb-item active">Dashboard</li>
-              </ol>
-            </div>
-          </div>
-        </div>
-      </div>
+<template>
+  <div class="content stimg">
+    <div class="content-header">
       <div class="container-fluid">
-        <div class="row">
-          <div class="col-lg-6">
-            <Responsive class="w-full">
-              <template #main="{ width }">
-                <Chart
-                  direction="circular"
-                  :size="{ width, height: 400 }"
-                  :data="data"
-                  :margin="{
-                    left: Math.round((width - 360)/2),
-                    top: 40,
-                    right: 0,
-                    bottom: 20
-                  }"
-                  :axis="axis"
-                  :config="{ controlHover: false }"
-                  >
-                  <template #layers>
-                    <Pie
-                      :dataKeys="['name', 'pl']"
-                      :pie-style="{ innerRadius: 100, padAngle: 0.05 }" />
-                  </template>
-                  <template #widgets>
-                    <Tooltip
-                      :config="{
-                        name: { },
-                        avg: { hide: true},
-                        pl: { label: 'value' },
-                        inc: { hide: true }
-                      }"
-                      hideLine
-                    />
-                  </template>
-                </Chart>
-              </template>
-            </Responsive>
+        <div class="row mb-2 bg-dark text-white">
+          <div class="col-sm-6">
+            <h1 class="m-0">Dashboard</h1>
           </div>
-          <div class="col-lg-6">
-          
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item text-white">Home</li>
+              <li class="breadcrumb-item text-white">Dashboard</li>
+            </ol>
           </div>
         </div>
       </div>
     </div>
-</template>
-
-<script>
-import { ref } from 'vue'
-import { Chart, Responsive, Pie, Tooltip } from 'vue3-charts'
-import { plByMonth } from './Data'
-
-    export default{
-        name:'Home',
-        components: { Chart, Responsive, Pie, Tooltip },
-        setup() {
-          const data = ref(plByMonth)
-
-          return { data }
-        }
-    }
-</script>
-
-<style>
-.axis{
-  display: none;
-}
-</style> -->
-
-<template>
-  <div class="content">
-   <div class="content-header">
-     <div class="container-fluid">
-       <div class="row mb-2">
-         <div class="col-sm-6">
-           <h1 class="m-0">Dashboard</h1>
-         </div>
-         <div class="col-sm-6">
-           <ol class="breadcrumb float-sm-right">
-             <li class="breadcrumb-item"><a href="#">Home</a></li>
-             <li class="breadcrumb-item active">Dashboard</li>
-           </ol>
-         </div>
-       </div>
-     </div>
-   </div>
-   <div class="container-fluid">
-     <div class="row">
-       <div class="col-lg-6">
+    <div class="container-fluid mh">
+      <div class="row">
+        <!-- First column with the chart -->
+        <div class="col-lg-4">
+          <apexchart
+            type="radialBar"
+            height="350"
+            :options="chartOptions"
+            :series="series"
+          />
+        </div>
         
-       </div>
-       <div class="col-lg-6">
-       
-       </div>
-     </div>
-   </div>
- </div>
+        <!-- <div class="col-sm-2 clear-flex"></div> -->
+        
+        <!-- Second column with donut chart -->
+        <div class="col-lg-8">
+          <!-- Added new Vue instance here for area chart -->
+          <apexchart type="area" height="350" :options="areaChartOptions" :series="areaSeries"></apexchart>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-// import ApexCharts from 'apexcharts'
- export default{
-     name:'Home',
-     props:{
-            msg:String
-        }
- }
+// Import VueApexCharts
+import VueApexCharts from 'vue3-apexcharts';
+
+export default {
+  name: 'Home',
+  components: {
+    apexchart: VueApexCharts
+  },
+  data() {
+    return {
+      // Radial Bar Chart Data
+      series: [44, 55, 67, 83],
+      chartOptions: {
+        chart: {
+          height: 350,
+          type: 'radialBar',
+        },
+        title: {
+          text: 'All Project', 
+          align: 'center', 
+          margin: 10,
+          offsetX: 0, 
+          offsetY: 0, 
+          style: {
+            fontSize: '18px',
+            fontWeight: 'bold',
+            color: '#263238',
+          }
+        },
+        plotOptions: {
+          radialBar: {
+            dataLabels: {
+              name: {
+                fontSize: '22px',
+              },
+              value: {
+                fontSize: '16px',
+              },
+              total: {
+                show: true,
+                label: 'Total',
+                formatter: function () {
+                  return 249;
+                }
+              }
+            }
+          }
+        },
+        labels: ['Apples', 'Oranges', 'Bananas', 'Berries'],
+      },
+      
+      // Area Chart Data
+      areaSeries: [{
+        name: 'series1',
+        data: [31, 40, 28, 51, 42, 109, 100]
+      }, {
+        name: 'series2',
+        data: [11, 32, 45, 32, 34, 52, 41]
+      }],
+      areaChartOptions: {
+        chart: {
+          height: 350,
+          type: 'area'
+        },
+        dataLabels: {
+          enabled: false
+        },
+        stroke: {
+          curve: 'smooth'
+        },
+        xaxis: {
+          type: 'datetime',
+          categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
+        },
+        tooltip: {
+          x: {
+            format: 'dd/MM/yy HH:mm'
+          },
+        },
+      }
+    };
+  }
+};
 </script>
 
-<style>
-.axis{
-display: none;
+<style scoped>
+.mh {
+  height: 100vh;
+  background-color: rgba(255, 255, 255, 0.813);
+  border-radius: 10px;
+}
+
+.stimg {
+  background-image: url('/public/assets/dist/img/bg4.jpg');
+  background-attachment: fixed;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center center;
 }
 </style>
